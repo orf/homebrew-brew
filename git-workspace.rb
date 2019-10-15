29dd7c50601297cd19232fa1b48b6982f81a02de
Tom Forbes <tom@tomforb.es>
2019-10-15 23:08:44 +0100

    Update git-workspace.rb

--- git-workspace.rb
+++ git-workspace.rb
@@ -1,9 +1,9 @@
 class GitWorkspace < Formula
   desc "Mirror your local git repositories from multiple providers"
   homepage "https://github.com/orf/git-workspace"
-  url "https://github.com/orf/git-workspace/releases/download/v0.1.8/git-workspace-Darwin-x86_64.tar.gz"
-  version "0.1.8"
-  sha256 "eb5bacd71cddc05d7fdda1986f6a3fa4f8c019cd28fbfe91b42c1c10b022eae6"
+  url "https://github.com/orf/git-workspace/releases/download/v0.1.9/git-workspace-Darwin-x86_64.tar.gz"
+  version "0.1.9"
+  sha256 "687440a23a0978e05fa20fc554ae95dddc43ffd288fae41042a13875fef15d84"
 
   depends_on "git-lfs"
 
