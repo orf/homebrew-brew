05f3497db0a62eba69f48718b0de6dc2bddda1e5
Tom Forbes <tom@tomforb.es>
2019-10-15 23:19:37 +0100

    Support Linux

--- git-workspace.rb
+++ git-workspace.rb
@@ -1,9 +1,14 @@
 class GitWorkspace < Formula
   desc "Mirror your local git repositories from multiple providers"
   homepage "https://github.com/orf/git-workspace"
-  url "https://github.com/orf/git-workspace/releases/download/v0.1.9/git-workspace-Darwin-x86_64.tar.gz"
+  if OS.mac?
+    url "https://github.com/orf/git-workspace/releases/download/v0.1.9/git-workspace-Darwin-x86_64.tar.gz"
+    sha256 "687440a23a0978e05fa20fc554ae95dddc43ffd288fae41042a13875fef15d84"
+  elsif OS.linux?
+    url "https://github.com/orf/git-workspace/releases/download/v0.1.9/git-workspace-Linux-x86_64.tar.gz"
+    sha256 "dd4af56d435656f3609d0f7fa4e4dee6fa9e74b5ee295f3ced4eee4ede8a4c11"
+  end
   version "0.1.9"
-  sha256 "687440a23a0978e05fa20fc554ae95dddc43ffd288fae41042a13875fef15d84"
 
   depends_on "git-lfs"
 
