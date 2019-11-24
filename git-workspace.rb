4182647db6a322dea76a25c096226769fcff7350
Tom Forbes <tom@tomforb.es>
2019-11-24 16:35:32 +0000

    Git workspace 0.4.0

--- git-workspace.rb
+++ git-workspace.rb
@@ -2,13 +2,13 @@ class GitWorkspace < Formula
   desc "Mirror your local git repositories from multiple providers"
   homepage "https://github.com/orf/git-workspace"
   if OS.mac?
-    url "https://github.com/orf/git-workspace/releases/download/v0.3.0/git-workspace-Darwin-x86_64.tar.gz"
-    sha256 "b1341dcffcf6da0d6989aab0e48644d7b18d4e3a2f73b7780fbef09bd120d2bc"
+    url "https://github.com/orf/git-workspace/releases/download/v0.4.0/git-workspace-Darwin-x86_64.tar.gz"
+    sha256 "4d23e682eaa2468b552ee1ffeb59d3fcb1b709da79813984698ba89c9b0548d9"
   elsif OS.linux?
-    url "https://github.com/orf/git-workspace/releases/download/v0.3.0/git-workspace-Linux-x86_64.tar.gz"
-    sha256 "0ca82b44e5cd43864ed6e3e19065e95107ef6726dcff4ebc788b247aeeaf90a2"
+    url "https://github.com/orf/git-workspace/releases/download/v0.4.0/git-workspace-Linux-x86_64.tar.gz"
+    sha256 "b667b6fbf1e712f53559c5b186e8cc5b563bff7b9ddfc232bac13b5261b1fcb8"
   end
-  version "0.3.0"
+  version "0.4.0"
 
   def install
     bin.install "git-workspace"
