b94c52a963fb81757b4cf42163e3c583834cedaa
Tom Forbes <tom@tomforb.es>
2020-03-09 19:37:36 +0000

    Update deterministic-zip.rb

--- deterministic-zip.rb
+++ deterministic-zip.rb
@@ -2,10 +2,10 @@ class DeterministicZip < Formula
   desc "Deterministic zipfiles, with Rust"
   homepage "https://github.com/orf/deterministic-zip"
   if OS.mac?
-    url "https://github.com/orf/deterministic-zip/releases/download/v0.0.3/deterministic-zip-Darwin-x86_64.tar.gz"
+    url "https://github.com/orf/deterministic-zip/releases/download/v0.0.4/deterministic-zip-Darwin-x86_64.tar.gz"
     sha256 "0eed9e5a5d3709b0b4170454cdef41cb0ae691794753e78b5eb4c83438fedb3e"
   elsif OS.linux?
-    url "https://github.com/orf/deterministic-zip/releases/download/v0.0.3/deterministic-zip-Linux-x86_64.tar.gz"
+    url "https://github.com/orf/deterministic-zip/releases/download/v0.0.4/deterministic-zip-Linux-x86_64.tar.gz"
     sha256 "94dd62810a1fad8d100c156540cebd777a2fa71108d54110287d97352ef9d7ae"
   end
   version "0.0.4"
