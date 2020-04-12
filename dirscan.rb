6ae2e2d00bcdae5190c03b57d9ab470f620a4f5c
Tom Forbes <tom@tomforb.es>
2020-04-12 12:15:41 +0100

    Release dirscan 0.3.0

--- dirscan.rb
+++ dirscan.rb
@@ -2,13 +2,13 @@ class Dirscan < Formula
   desc "Analyse large drives or directories for insights, fast"
   homepage "https://github.com/orf/ptail"
   if OS.mac?
-    url "https://github.com/orf/dirscan/releases/download/v0.2.0/dirscan-Darwin-x86_64.tar.gz"
-    sha256 "97f9170fa7fc1b3e9fdd72b11c200f4801ecfb7ec5af5198c998d5dbc046ca18"
+    url "https://github.com/orf/dirscan/releases/download/v0.3.0/dirscan-Darwin-x86_64.tar.gz"
+    sha256 "3b35fcaf6482fc3be134bfb9d20f719969420ca85406a9db99a500b1e4cb4600"
   elsif OS.linux?
-    url "https://github.com/orf/dirscan/releases/download/v0.2.0/dirscan-Linux-x86_64.tar.gz"
-    sha256 "c8bacb27c5ea4a1fd43907323bdd6f67bff4f29b2e3e3ce10f553378bcb9b516"
+    url "https://github.com/orf/dirscan/releases/download/v0.3.0/dirscan-Linux-x86_64.tar.gz"
+    sha256 "ade06c671c654aa75a006ce233bb0ce89258d053c9c78ddae212207e89718624"
   end
-  version "0.2.0"
+  version "0.3.0"
 
   def install
     bin.install "dirscan"
