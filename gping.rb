d7125166d0353d75af46bd4dd0597e63a4b3282b
Tom Forbes <tom@tomforb.es>
2020-11-13 23:22:17 +0000

    Update gping.rb

--- gping.rb
+++ gping.rb
@@ -2,13 +2,13 @@ class Gping < Formula
   desc "Ping, but with a graph."
   homepage "https://github.com/orf/gping"
   if OS.mac?
-    url "https://github.com/orf/gping/releases/download/v0.1.5/gping-Darwin-x86_64.tar.gz"
-    sha256 "c92d74c65e9038202d9cf6c07f011236ad250e65c2eb19e4677a795b4d9d8273"
+    url "https://github.com/orf/gping/releases/download/v0.1.6/gping-Darwin-x86_64.tar.gz"
+    sha256 "52887b7bed78afe253a8c514e43d8825f4707897a5cb081cc0c30fc5e78d97e5"
   elsif OS.linux?
-    url "https://github.com/orf/gping/releases/download/v0.1.5/gping-Linux-x86_64.tar.gz"
-    sha256 "fd56dd90a5785c0ca4ce1f46e50b0d709a75fcf7a161174bebfcbc110c8fe2da"
+    url "https://github.com/orf/gping/releases/download/v0.1.6/gping-Linux-x86_64.tar.gz"
+    sha256 "167d6d534c218ffa4a8b4e8db9bf9d58fc2ccf5dd38931a7349913acea4164b6"
   end
-  version "0.1.5"
+  version "0.1.6"
 
   def install
     bin.install "gping"
