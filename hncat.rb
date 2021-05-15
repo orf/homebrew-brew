99d583215950c2e5ec838072b8d89016119d0a4d
Tom Forbes <tom@tomforb.es>
2021-05-15 15:43:12 +0100

    Hncat 0.1.3

--- hncat.rb
+++ hncat.rb
@@ -2,13 +2,13 @@ class Hncat < Formula
   desc "Grab all Hacker News stores + comments, quickly. "
   homepage "https://github.com/orf/hncat"
   if OS.mac?
-    url "https://github.com/orf/hncat/releases/download/v0.1.1/hncat-Darwin-x86_64.tar.gz"
-    sha256 "7d45f2eeee2f5a3808d543ddd6d073bc5f1f6620aadeca959070f0d7a0c1de9d"
+    url "https://github.com/orf/hncat/releases/download/v0.1.3/hncat-Darwin-x86_64.tar.gz"
+    sha256 "0d8198f569d3bd294dcc036fefed0987ab39a28d2666d1080c6541879a3ca24f"
   elsif OS.linux?
-    url "https://github.com/orf/hncat/releases/download/v1.1.1/hncat-Linux-x86_64.tar.gz"
-    sha256 "6cd707ff8e96e9889a0a83fcf349ef751d71ceef6c2e51905b86bf5a5e0a036a"
+    url "https://github.com/orf/hncat/releases/download/v1.1.3/hncat-Linux-x86_64.tar.gz"
+    sha256 "e01fa512aaa125df8cf11b848517532e12fd0bbb89174928bbfe796b83ac83ac"
   end
-  version "0.1.1"
+  version "0.1.3"
 
   def install
     bin.install "hncat"
